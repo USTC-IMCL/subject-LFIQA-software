c5dc6d30f66ec1488ac74bfb3bbd91a26c9ab6d4
= <=>
2024-08-16 00:14:08 +0800

    update

--- Widgets/ProjectDisplay.py
+++ Widgets/ProjectDisplay.py
@@ -11,7 +11,7 @@ class ProjectMenuLabel(QtWidgets.QFrame):
     '''
     A box containing an icon
     '''
-    clicked = QtCore.Signal()
+    clicked = QtCore.Signal(int)
     def __init__(self,index,*args, **kwargs):
         super().__init__(*args, **kwargs)
         self.index=index
@@ -34,15 +34,15 @@ class ProjectMenuLabel(QtWidgets.QFrame):
             self.animation=True
     
     def mousePressEvent(self, event):
-        self.setStyleSheet("background-color: lightblue;")
+        self.setStyleSheet("background-color: gray;")
         self.is_selected=True
         if self.animation:
             self.icon_label.setPixmap(QtGui.QPixmap(self.active_icon).scaled(self.icon_width,self.icon_height))
-        self.clicked.emit()
+        self.clicked.emit(self.index)
 
     def DeActive(self):
         self.is_selected=False
-        self.setStyleSheet("background-color: white;")
+        self.setStyleSheet("background-color: lightgray;")
         if self.animation:
             self.icon_label.setPixmap(QtGui.QPixmap(self.deactive_icon).scaled(self.icon_width,self.icon_height))
 
@@ -56,7 +56,8 @@ class ProjectDisplay(QtWidgets.QFrame):
         #self.h_splitter=QtWidgets.QSplitter(QtCore.Qt.Orientation.Horizontal,self)
         #self.main_layout.addWidget(self.h_splitter)
 
-        self.left_panel=QtWidgets.QScrollArea(self)
+        self.left_panel=QtWidgets.QWidget(self)
+        self.left_panel.setStyleSheet("background-color: lightgray;")
         self.left_panel.setSizePolicy(QSizePolicy.Policy.Fixed,QSizePolicy.Policy.Expanding)
         self.left_panel.setFixedWidth(150)
         self.main_layout.addWidget(self.left_panel)
@@ -65,34 +66,79 @@ class ProjectDisplay(QtWidgets.QFrame):
         self.right_stack.setSizePolicy(QtWidgets.QSizePolicy.Policy.Expanding,QtWidgets.QSizePolicy.Policy.Expanding)
         self.main_layout.addWidget(self.right_stack)
 
-        self.right_stack.addWidget(QtWidgets.QLabel("test"))
+        self.main_layout.setStretch(1,1)
 
-        self.MakeLeftPanel()
+        self.right_stack.addWidget(QtWidgets.QWidget())
+
         self.MakeRightPanel()
+        self.MakeLeftPanel()
 
     def MakeLeftPanel(self):
         self.left_panel_layout=QtWidgets.QVBoxLayout()
         self.left_panel_layout.setContentsMargins(0,0,0,0)
         self.left_panel_layout.setSpacing(3)
+        self.left_panel_layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop | QtCore.Qt.AlignmentFlag.AlignHCenter)
+        self.left_panel.setLayout(self.left_panel_layout)
 
-        panel_width=self.left_panel.width()-2*v_space
+        panel_width=self.left_panel.width()
 
-        self.label_material=ProjectMenuLabel(0,self.left_panel)
-        self.label_material.setGeometry(v_space,v_space,panel_width,panel_width)
+        self.all_menu_labels=[]
+        self.label_material=ProjectMenuLabel(0)
+        self.label_material.setFixedSize(panel_width,panel_width)
+        self.left_panel_layout.addWidget(self.label_material)
         self.label_material.setToolTip("The training and testing material")
         self.label_material.SetIcons(u":/icons/res/folder_close.svg",u":/icons/res/folder_open.svg")
+        self.label_material.clicked.connect(lambda index: self.ActivateMenuLabel(index))
+        self.all_menu_labels.append(self.label_material)
 
-        self.label_setting=ProjectMenuLabel(1,self.left_panel)
+        self.label_setting=ProjectMenuLabel(1)
         self.label_setting.setFixedSize(panel_width,panel_width)
         self.label_setting.setToolTip('Setting the project and experiment')
         self.label_setting.SetIcons(u':/icons/res/setting.svg')
+        self.left_panel_layout.addWidget(self.label_setting)
+        self.label_setting.clicked.connect(lambda index: self.ActivateMenuLabel(index))
+        self.all_menu_labels.append(self.label_setting)
 
-        self.label_subjects=ProjectMenuLabel(2,self.left_panel)
+        self.label_subjects=ProjectMenuLabel(2)
+        self.label_subjects.setFixedSize(panel_width,panel_width)
         self.label_subjects.SetIcons(u':/icons/res/subject.png')
         self.label_subjects.setToolTip("Manage your subjects here.")
+        self.label_subjects.clicked.connect(lambda index: self.ActivateMenuLabel(index))
+        self.left_panel_layout.addWidget(self.label_subjects)
+        self.all_menu_labels.append(self.label_subjects)
 
     def MakeRightPanel(self):
-        pass
+        self.MakeMaterialWidget()
+        self.MakeSettingWidget()
+
+    def MakeMaterialWidget(self):
+        right_stack_width=self.right_stack.width()
+        right_stack_height=self.right_stack.height()
+
+        self.material_widget=QtWidgets.QTabWidget()
+        self.right_stack.addWidget(self.material_widget)
+        self.material_widget.setGeometry(0,0,right_stack_width,right_stack_height)
+
+        self.material_widget.addTab(QtWidgets.QFrame(),u"Training")
+        self.material_widget.addTab(QtWidgets.QFrame(),u"Testing")
+
+    def MakeSettingWidget(self):
+        self.right_stack.addWidget(QtWidgets.QFrame())
+        cur_widget=self.right_stack.widget(2) #currentWidget()
+        cur_widget.setStyleSheet("background-color: lightblue;")
+        
+    def MakeSubjectsWidget(self):
+        self.right_stack.addWidget(QtWidgets.QFrame())
+
+
+    def ActivateMenuLabel(self,index):
+        self.right_stack.setCurrentIndex(index+1)
+        for label in self.all_menu_labels:
+            if label.index==index:
+                continue
+            else:
+                label.DeActive()
+
 
 if __name__ == "__main__":
     app=QtWidgets.QApplication()
