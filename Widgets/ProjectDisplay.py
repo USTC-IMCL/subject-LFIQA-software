6e5d41dbf17fc2304b2fb3e768131a09ed38b8ae
Shengyang <zsy7788@mail.ustc.edu.cn>
2024-09-13 18:12:42 +0800

    submit to main

--- Widgets/ProjectDisplay.py
+++ Widgets/ProjectDisplay.py
@@ -28,6 +28,9 @@ class ExpSettingWidget(QtWidgets.QFrame):
         # scoring control
 
 
+
+
+
 class NewLFISelector(QtWidgets.QFrame):
     on_cancel=QtCore.Signal()
     on_confirm=QtCore.Signal(dict)
