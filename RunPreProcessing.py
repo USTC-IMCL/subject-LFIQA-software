8310b25ac728a9dab21e8ee9665be603c9b90a58
= <=>
2025-01-08 14:11:11 +0800

    update

--- RunPreProcessing.py
+++ RunPreProcessing.py
@@ -2,4 +2,20 @@ import os
 import sys
 sys.path.append('./Utils')
 import PreProcess
+import DenseRefocusFunction
+import numpy as np
+import cv2
+import json
+
+lambda_file=None
+meta_data=None
+refocusing_folder=None
+post_fix='.png'
+angular_height=11
+angular_width=11
+
+def CalDenseRefocusing(img_path):
+    test_img=os.path.join(img_path,'001_001.png')
+    test_img=cv2.imread(test_img,-1)
+    lf_image=np.zeros(angular_height,angular_width,)
 
